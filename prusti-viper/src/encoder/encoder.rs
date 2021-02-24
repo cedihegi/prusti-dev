302cac3563acc84385c6f20cec3ade5942f0f513
cedric <hegglinc@student.ethz.ch>
2021-02-24 21:32:45 +0100

    Backtranslate counterexample for some types commit prints a lot of unnecessary messages and still contains all the counterexample-testcases from the early phase of this thesis, will be removed / split up into two branches

--- prusti-viper/src/encoder/encoder.rs
+++ prusti-viper/src/encoder/encoder.rs
@@ -56,6 +56,9 @@ use crate::encoder::utils::transpose;
 use crate::encoder::errors::EncodingResult;
 use crate::encoder::errors::SpannedEncodingResult;
 use crate::encoder::snapshot;
+use crate::encoder::counterexample::*;
+use viper::SiliconCounterexample;
+use crate::encoder::places::LocalVariableManager;
 
 const SNAPSHOT_MIRROR_DOMAIN: &str = "$SnapshotMirrors$";
 
@@ -223,10 +226,6 @@ impl<'v, 'tcx> Encoder<'v, 'tcx> {
             functions: self.get_used_viper_functions(),
             viper_predicates: self.get_used_viper_predicates(),
         }
-    }
-
-    pub fn get_cfg_method(&self, def_id: ProcedureDefId) -> Option<vir::CfgMethod>{
-       self.procedures.borrow_mut().get(&def_id).map(|x| (*x).clone())
     }
 
     pub(in crate::encoder) fn register_encoding_error(&self, encoding_error: SpannedEncodingError) {
@@ -1684,6 +1683,34 @@ impl<'v, 'tcx> Encoder<'v, 'tcx> {
             }
         };
         result
+    }
+
+    pub fn get_cfg_method(&self, def_id: ProcedureDefId) -> Option<vir::CfgMethod>{
+       self.procedures.borrow_mut().get(&def_id).map(|x| (*x).clone())
+    }
+
+    pub fn get_counterexample(
+        &self, 
+        def_id: ProcedureDefId,
+        silicon_counterexample: Option<SiliconCounterexample>
+    ) -> Counterexample {
+        let procedure = self.env.get_procedure(def_id);
+        let methods = self.procedures.borrow();
+        let cfg_method_opt = methods.get(&def_id);
+        if let Some(cfg) = cfg_method_opt{
+            println!("return label {:?}", cfg.get_return_label());
+        }
+        let mir = procedure.get_mir();
+        let tyctxt = &self.env.tcx();
+        let is_pure = self.is_pure(def_id);
+        /*
+        let var_debug_info = mir.var_debug_info.clone();
+        let locals = mir.local_decls.clone();
+        let arg_count = mir.arg_count;
+        let local_manager = LocalVariableManager::new(&locals); 
+        */
+
+        backtranslate(silicon_counterexample, mir, is_pure, tyctxt)
     }
 }
 
